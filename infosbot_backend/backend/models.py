7964af8ffc88e72eb6c665957c2553ce41e4ca41
Jannes Höke <mail@jannes-hoeke.de>
2017-03-21 13:10:20 +0100

    :tada: Initial commit for django app

---
+++ infosbot_backend/backend/models.py
@@ -0,0 +1,17 @@
+import datetime
+
+from django.db import models
+
+
+class Info(models.Model):
+    headline = models.CharField('Schlagzeile', max_length=200, null=False)
+    intro_text = models.CharField('Intro-Text', max_length=200, null=False)
+    first_question = models.CharField('Erste Frage', max_length=20, null=True, blank=True)
+    first_text = models.CharField('Erster Text', max_length=600, null=True, blank=True)
+    second_question = models.CharField('Zweite Frage', max_length=20, null=True, blank=True)
+    second_text = models.CharField('Zweiter Text', max_length=600, null=True, blank=True)
+    media = models.FileField('Medien-Anhang', null=True, blank=True)
+    pub_date = models.DateTimeField('Veröffentlicht am', default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.headline
---
+++ infosbot_backend/start.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "infosbot_backend.settings"
+
+import cherrypy
+import django
+django.setup()
+
+from django.conf import settings
+from django.core.handlers.wsgi import WSGIHandler
+
+
+class DjangoApplication(object):
+    HOST = "127.0.0.1"
+    PORT = 8001
+
+    def mount_static(self, url, root):
+        """
+        :param url: Relative url
+        :param root: Path to static files root
+        """
+        config = {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': root,
+            'tools.expires.on': True,
+            'tools.expires.secs': 86400
+        }
+        cherrypy.tree.mount(None, url, {'/': config})
+
+    def run(self):
+        cherrypy.config.update({
+            'environment': 'production',
+            'server.socket_host': self.HOST,
+            'server.socket_port': self.PORT,
+            'engine.autoreload_on': False,
+            'log.error_file': 'site.log',
+            'log.screen': True
+        })
+        self.mount_static(settings.STATIC_URL, settings.STATIC_ROOT)
+
+        cherrypy.log("Loading and serving Django application")
+        cherrypy.tree.graft(WSGIHandler(), '/backend')
+        cherrypy.engine.start()
+        cherrypy.log("Your app is running at http://%s:%s" % (self.HOST, self.PORT))
+
+        cherrypy.engine.block()
+
+
+if __name__ == "__main__":
+    DjangoApplication().run()
