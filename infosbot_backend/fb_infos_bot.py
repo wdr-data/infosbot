b8709522baa09f39a5d7507841c3a9f84ff219e0
Jannes Höke <mail@jannes-hoeke.de>
2017-04-03 20:59:17 +0200

    :art: logging, pep8, environ

--- infosbot_backend/fb_infos_bot.py
+++ infosbot_backend/fb_infos_bot.py
@@ -1,35 +1,37 @@
-from flask import Flask, request
+import json
+import logging
 from os.path import basename, isfile, join
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
-from bs4 import BeautifulSoup
-from lxml import html
 import sqlite3 as lite
-import requests
 import datetime
 import re
+import os
 
-import json
-import logging
+from flask import Flask, request
+from bs4 import BeautifulSoup
+from lxml import html
+import requests
 
- #Enable logging
+# Enable logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO, filename='infos_logger.log', filemode='a')
+                    level=logging.DEBUG)
 
-logger.setLevel(logging.DEBUG)
-logger.debug('fb Infos Bot Logging')
+logger.info('FB Infos Bot Logging')
 
 SEARCH, SELECT = range(2)
 
-PAGE_TOKEN=""
+PAGE_TOKEN = os.environ['INFOSBOT_PAGE_TOKEN']
+HUB_VERIFY_TOKEN = os.environ['INFOSBOT_HUB_VERIFY_TOKEN']
 infos = list()
 
-app = Flask(__name__, static_folder='.well-known')
+app = Flask(__name__)
+
 
 @app.route('/testbot', methods=["GET"])
 def confirm():
-    if request.args['hub.verify_token'] == "":
+    if request.args['hub.verify_token'] == HUB_VERIFY_TOKEN:
         return request.args['hub.challenge']
     return 'Hello World!'
 
@@ -40,16 +42,17 @@ def receive_message():
     handle_messages(data)
     return "ok"
 
+
 def handle_messages(data):
     """handle all incoming messages"""
     messaging_events = data['entry'][0]['messaging']
-    print(messaging_events)
+    logger.debug(messaging_events)
     for event in messaging_events:
         sender_id = event['sender']['id']
 
         # check if we actually have some input
-        if "message" in event and event['message'].get("text","") != "":
-            print('received message')
+        if "message" in event and event['message'].get("text", "") != "":
+            logger.debug('received message')
             text = event['message']['text']
             if text == '/info':
                 reply = "Heute haben wir folgende Themen für dich:\n"
@@ -57,16 +60,20 @@ def handle_messages(data):
                 data = get_data()
                 send_list_template(data, sender_id)
             else:
-                reply = "echo: " +text
+                reply = "echo: " + text
                 send_text(sender_id, reply)
-        elif "postback" in event and event['postback'].get("payload","") == "start":
-            reply = "Herzlich willkommen zum 1LIVE InfoMessenger. \n\nHier bekommst Du alle Infos geliefert, die Du wissen musst, um mitreden zu können, selbst die, von denen Du nicht weißt, dass Du sie wissen wolltest :) \n\nWas Du dafür tun musst: Fast nichts. Tippe \"/info\" um dein Update zu bekommen."
+        elif "postback" in event and event['postback'].get("payload", "") == "start":
+            reply = "Herzlich willkommen zum 1LIVE InfoMessenger. \n\n" \
+                    "Hier bekommst Du alle Infos geliefert, die Du wissen musst, um mitreden zu " \
+                    "können, selbst die, von denen Du nicht weißt, dass Du sie wissen wolltest" \
+                    " :) \n\nWas Du dafür tun musst: Fast nichts. Tippe \"/info\" um dein " \
+                    "Update zu bekommen."
             send_text(sender_id, reply)
-        elif "postback" in event and event['postback'].get("payload","").split("#")[0] == "info":
+        elif "postback" in event and event['postback'].get("payload", "").split("#")[0] == "info":
             for info in infos:
                 #reply = event['postback'].get("payload","").split("#")[1]
                 #reply += info[0]
-                if event['postback'].get("payload","").split("#")[1] == str(info[0]):
+                if event['postback'].get("payload", "").split("#")[1] == str(info[0]):
                     reply = info[2]
                     send_text(sender_id, reply)
         #     audio_url = event['postback'].get("payload","").split("#")[1]
@@ -74,6 +81,7 @@ def handle_messages(data):
         #     send_audio(sender_id, audio_file)
             #reply = "Zukünftig wird dir hier ein Audio geschickt..."
             #send_text(sender_id, reply)
+
 
 def get_data():
     db_path = 'infosbot_backend/db.sqlite3'
@@ -87,137 +95,143 @@ def get_data():
         while True:
             row = cur.fetchone()
 
-            if row == None:
+            if row is None:
                 break
-        ### heute abfragen und in dict ablegen... ###
+            # heute abfragen und in dict ablegen...
             if row[7].split()[0] == '2017-04-03':
                 infos.append(row)
 
-        #logger.debug (info)
+        #logger.debug(info)
     return infos
+
 
 def send_text(recipient_id, text):
     """send a text message to a recipient"""
-    recipient = { 'id' : recipient_id }
-    message = { 'text' : text }
+    recipient = {'id': recipient_id}
+    message = {'text': text}
     payload = {
-        'recipient' : recipient,
-        'message' : message
+        'recipient': recipient,
+        'message': message
     }
     send(payload)
+
 
 def send_image(recipient_id, image_url):
     """send an image to a recipient"""
 
-    recipient = { 'id' : recipient_id }
+    recipient = {'id': recipient_id}
 
     # create an image object
-    image = { 'url' : image_url }
+    image = {'url': image_url}
 
     # add the image object to an attachment of type "image"
     attachment = {
-        'type'      : 'image',
-        'payload'   : image
+        'type': 'image',
+        'payload': image
     }
 
     # add the attachment to a message instead of "text"
-    message = { 'attachment' : attachment }
+    message = {'attachment': attachment}
 
     # now create the final payload with the recipient
     payload = {
-        'recipient' : recipient,
-        'message' : message
+        'recipient': recipient,
+        'message': message
     }
     send(payload)
+
 
 def send_audio(recipient_id, audio_file):
     """send an audio to a recipient"""
     audio_file = "https://mediandr-a.akamaihd.net/progressive/2017/0302/AU-20170302-0656-0300.mp3"
 
-    recipient = { "id" : recipient_id }
-    audio = { 'url' : audio_file }
-    filedata= '@' + audio_file + ';type=audio/mp3'
+    recipient = {"id": recipient_id}
+    audio = {'url': audio_file}
+    filedata = '@' + audio_file + ';type=audio/mp3'
 
     attachment = {
-        'type' : 'audio',
-        'payload' : audio
+        'type': 'audio',
+        'payload': audio
     }
 
-    message = { 'attachment' : attachment }
+    message = {'attachment': attachment}
 
     payload = {
-        'recipient' : recipient,
-        'message' : message,
-        'filedata' : filedata
+        'recipient': recipient,
+        'message': message,
+        'filedata': filedata
     }
     send(payload)
+
 
 def send_generic_template(recipient_id, gifts):
     """send a generic message with title, text, image and buttons"""
     selection = []
 
     for key in gifts:
-        print(key)
+        logger.debug(key)
         gift = key
 
         title = gifts[gift]['title']
-        print (title)
+        logger.debug(title)
         item_url = gifts[gift]['link']
         image_url = 'http://www1.wdr.de/mediathek/audio/sendereihen-bilder/wdr_sendereihenbild100~_v-Podcast.jpg'
         subtitle = gifts[gift]['teaser']
+
         listen_Button = {
-            'type' : 'postback',
-            'title' : 'ZeitZeichen anhören',
-            'payload' : 'listen_audio#' + item_url
+            'type': 'postback',
+            'title': 'ZeitZeichen anhören',
+            'payload': 'listen_audio#' + item_url
         }
         download_Button = {
-            'type' : 'web_url',
-            'title' : 'ZeitZeichen herunterladen',
-            'url' : item_url
+            'type': 'web_url',
+            'title': 'ZeitZeichen herunterladen',
+            'url': item_url
         }
         visit_Button = {
-            'type' : 'web_url',
-            'url' : 'http://www1.wdr.de/radio/wdr5/sendungen/zeitzeichen/index.html',
-            'title' : 'Zum WDR ZeitZeichen'
+            'type': 'web_url',
+            'url': 'http://www1.wdr.de/radio/wdr5/sendungen/zeitzeichen/index.html',
+            'title': 'Zum WDR ZeitZeichen'
         }
         share_Button = {
-            'type' : 'element_share'
+            'type': 'element_share'
         }
-	### Buttons sind auf max 3 begrenzt! ###
+        ### Buttons sind auf max 3 begrenzt! ###
         buttons = []
         buttons.append(listen_Button)
         buttons.append(download_Button)
         buttons.append(share_Button)
 
         elements = {
-            'title' : title,
-            'item_url' : item_url,
-            'image_url' : image_url,
-            'subtitle' : subtitle,
-            'buttons' : buttons
+            'title': title,
+            'item_url': item_url,
+            'image_url': image_url,
+            'subtitle': subtitle,
+            'buttons': buttons
         }
 
         selection.append(elements)
 
     load = {
-            'template_type' : 'generic',
-            'elements' : selection
+            'template_type': 'generic',
+            'elements': selection
         }
 
     attachment = {
-        'type' : 'template',
-        'payload' : load
+        'type': 'template',
+        'payload': load
     }
 
-    message = { 'attachment' : attachment }
+    message = {'attachment': attachment}
 
-    recipient = { 'id' : recipient_id }
+    recipient = {'id': recipient_id}
 
     payload = {
-        'recipient' : recipient,
-        'message' : message
+        'recipient': recipient,
+        'message': message
     }
     send(payload)
+
 
 def send_list_template(infos, recipient_id):
     """send a generic message with a list of choosable informations"""
@@ -225,50 +239,52 @@ def send_list_template(infos, recipient_id):
 
     for info in infos:
         title = info[1]
-        print (title)
+        logger.debug(title)
         button = {
-            'type' : 'postback',
-            'title' : 'Mehr dazu',
-            'payload' : 'info#' + str(info[0])
+            'type': 'postback',
+            'title': 'Mehr dazu',
+            'payload': 'info#' + str(info[0])
         }
         buttons = []
         buttons.append(button)
 
         elements = {
-            'title' : title,
-            'buttons' : buttons
+            'title': title,
+            'buttons': buttons
         }
 
         selection.append(elements)
 
     load = {
-            'template_type' : 'list',
+            'template_type': 'list',
             'top_element_style': 'compact',
-            'elements' : selection
+            'elements': selection
         }
 
     attachment = {
-        'type' : 'template',
-        'payload' : load
+        'type': 'template',
+        'payload': load
     }
 
-    message = { 'attachment' : attachment }
+    message = {'attachment': attachment}
 
-    recipient = { 'id' : recipient_id }
+    recipient = {'id': recipient_id}
 
     payload = {
-        'recipient' : recipient,
-        'message' : message
+        'recipient': recipient,
+        'message': message
     }
     send(payload)
 
+
 def send(payload):
     """send a payload via the graph API"""
-    print(json.dumps(payload))
+    logger.debug(json.dumps(payload))
     headers = {'Content-Type': 'application/json'}
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token="+PAGE_TOKEN,
-        data = json.dumps(payload),
-        headers = headers)
+    requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + PAGE_TOKEN,
+                  data=json.dumps(payload),
+                  headers=headers)
+
 
 if __name__ == '__main__':
     #update_data()
