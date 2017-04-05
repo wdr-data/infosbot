1b78b43ec68eff9141c1283c8f63bc46466c8a58
Jannes Höke <mail@jannes-hoeke.de>
2017-04-05 13:48:45 +0200

    :lipstick: Use TextArea for text input in admin backend

--- infosbot_backend/backend/admin.py
+++ infosbot_backend/backend/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
+from django import forms
 from .models import Info
 
+
+class InfoModelForm(forms.ModelForm):
+    intro_text = forms.CharField(widget=forms.Textarea)
+    first_text = forms.CharField(widget=forms.Textarea)
+    second_text = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = Info
+        fields = '__all__'
+
+
+class InfoAdmin(admin.ModelAdmin):
+    form = InfoModelForm
+
 # Register your models here.
-admin.site.register(Info)
+admin.site.register(Info, InfoAdmin)
