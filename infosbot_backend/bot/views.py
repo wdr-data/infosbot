194d401ce223bd1130b97f1484c5d3731dc1651b
Jannes Höke <jhoeke@uni-bremen.de>
2017-05-22 11:25:32 +0200

    :bug: Use HttpResponse instead of strings

--- infosbot_backend/bot/views.py
+++ infosbot_backend/bot/views.py
@@ -2,6 +2,7 @@ import os
 import json
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import bot
 
@@ -11,13 +12,13 @@ HUB_VERIFY_TOKEN = os.environ.get('INFOSBOT_HUB_VERIFY_TOKEN', 'na')
 def webhook(request):
     if request.method == 'GET':
         if request.GET.get('hub.verify_token') == HUB_VERIFY_TOKEN:
-            return request.GET['hub.challenge']
+            return HttpResponse(request.GET['hub.challenge'], content_type="text/plain")
         else:
-            return 'Hello World!'
+            return HttpResponse('Hello World!', content_type="text/plain")
 
     elif request.method == 'POST':
         data = json.load(request.body.decode())
         bot.handle_messages(data)
-        return "ok"
+        return HttpResponse("ok", content_type="text/plain")
 
 
