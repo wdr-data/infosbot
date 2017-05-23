37e8af5535503faf009ad2b30ebb946bd2cdca06
LizzAy <lisachenbach@gmx.de>
2017-05-23 09:29:29 +0200

    :bug: changed url of picture
    changed url of picture

--- infosbot_backend/bot/bot.py
+++ infosbot_backend/bot/bot.py
@@ -140,19 +140,19 @@ def send_info(user_id, data, status):
         reply = data.intro_text
         button_title = data.first_question
         if data.intro_media != "":
-            image = "https://infos.data.wdr.de/backend/static/media/" + str(data.intro_media)
+            image = "https://infos.data.wdr.de/static/media/" + str(data.intro_media)
     elif status == "one":
         status_id = 'two'
         reply = data.first_text
         button_title = data.second_question
         if data.first_media != "":
-            image = "https://infos.data.wdr.de/backend/static/media/" + str(data.first_media)
+            image = "https://infos.data.wdr.de/static/media/" + str(data.first_media)
     elif status == "two":
         status_id = 'next'
         reply = data.second_text
         button_title = "None"
         if data.second_media != "":
-            image = "https://infos.data.wdr.de/backend/static/media/" + str(data.second_media)
+            image = "https://infos.data.wdr.de/static/media/" + str(data.second_media)
 
     quickreplies = []
     more_button = {
